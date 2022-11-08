1038a02ee1a46cd746c21532cf4f0fa77e407365
rusnnee <rj320@ic.ac.uk>
2022-11-08 17:19:12 +0000

    Ex.1

---
+++ src/main/java/MyServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns={"/patients","/doctors"},loadOnStartup = 1)
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.setContentType("text/html");
+    resp.getWriter().write(req.getServletPath());
+
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+}
+
+
+
